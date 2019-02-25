c521dfb6cbb176b420dcdadd19ce3db1c7f900cf
simple <8203877@qq.com>
2019-02-25 23:33:39 +0800

    file add

--- src/test_xueqiu.py
+++ src/test_xueqiu.py
@@ -19,27 +19,26 @@ class TestXueQiu:
                                         "autoGrantPermissions":True
                                         })
         self.driver.implicitly_wait(6)
+        self.find(By.XPATH,"//*[@text='允许']")
+        self.find(By.XPATH,"//*[@text='允许']")
         yield
         self.driver.quit()
 
     def find(self, id, value):
-        for i in range(3):
-            try:
-                if self.driver.find_element(id, value):
-                    return self.driver.find_element(id, value)
-                    break
-            except:
-                whitelist = ["//*[@text='允许']", "//*[@text='创建您的专属选股策略']",
-                             "//*[@text='下次再说']", "//*[contains(@resource-id,'iv_close')]"]
-                for key in whitelist:
-                    print(key)
-                    if self.driver.find_element_by_xpath(key):
-                        self.driver.find_element_by_xpath(key).click()
+        try:
+            self.driver.find_element(id, value)
+        except:
+            whitelist = ["//*[@text='允许']", "//*[@text='创建您的专属选股策略']",
+                         "//*[@text='下次再说']", "//*[contains(@resource-id,'iv_close')]"]
+            for key in whitelist:
+                print(key)
+                if self.driver.find_element_by_xpath(key):
+                    self.driver.find_element_by_xpath(key).click()
 
     def loaded(self):
         locations = []
         while True:
-            loc = self.find(By.XPATH,"//*[@text='自选' and contains(@resource-id,'tab_name')]")
+            loc = self.driver.find_element_by_xpath("//*[@text='自选' and contains(@resource-id,'tab_name')]")
             print(loc)
             locations.append(loc.location)
             if len(locations) >= 2:
@@ -51,9 +50,9 @@ class TestXueQiu:
         self.loaded()
         print("@"*20)
         self.driver.find_element(By.XPATH, "//*[@text='自选' and contains(@resource-id,'tab_name')]").click()
-        self.driver.find_element(By.ID,"tv_search")
+        self.driver.find_element(By.ID,"tv_search").click()
         self.driver.find_element_by_id("search_input_text").send_keys("pdd")
-        self.driver.find_element(By.XPATH,"//*[@text='拼多多']")
+        self.driver.find_element(By.XPATH,"//*[@text='拼多多']").click()
         assert self.driver.find_element_by_xpath("//*[@text='拼多多']") \
                and self.driver.find_element_by_xpath("//*[@text='美股开户']")
 
